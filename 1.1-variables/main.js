4a69e48ea3cc08e009a1e1000843dae4e3cd02c3
Nikita Dedkov <dedkovnd@gmail.com>
2020-01-21 15:28:22 +0300

    first commit

---
+++ 1.1-variables/main.js
@@ -0,0 +1,17 @@
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+
+let medianScore = (algebra + geography + physics)/3;
+console.log(medianScore);
+
+let myName = "Nick";
+let message = `Привет, мир! Меня зовут ${myName}`;
+console.log(message);
+
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = x * y + 5 * z + x - 1;
+console.log(result);
