18e39d3d6280aa07c4800e207c80f4ba2c3c9a6d
Nikita Dedkov <dedkovnd@gmail.com>
2020-01-26 21:57:26 +0300

    first commit

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,4 +1,4 @@
-
+"use strict";
 
 function calculateQuadraticEquation(){
     let a = +window.a.value;
@@ -10,9 +10,18 @@ function calculateQuadraticEquation(){
     span.textContent = "х = "+result;
 }
 
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    //return x;
+function getResult(a,b,c) {
+    let d = b*b - 4*a*c;
+    let x = [];
+
+    if (d > 0) {
+        x = [-b + Math.sqrt(d)/2*a, -b - Math.sqrt(d)/2*a]
+    } else if (d == 0) {
+        x = [-b/2*a]
+    } else {
+        x = ["нет корней"]
+    }
+    return x;
 }
 
 function calculateAverageRating(){
@@ -22,8 +31,17 @@ function calculateAverageRating(){
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    //return averageMark;
+    let sum = 0;
+    if (marks.length > 5) {
+        marks.splice(5);
+    }
+    for (let i = 0; i < marks.length; i++) {
+       sum += marks[i];
+    }
+    marks = sum/marks.length;
+
+    return marks;
+
 }
 
 function calculateDrinkTask(){
@@ -34,7 +52,13 @@ function calculateDrinkTask(){
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    //console.log(result)
-    //return result;
+    let day = new Date;
+    let year = day.getFullYear();
+    const yearDrink = dateOfBirthday.getFullYear();
+    if (year - yearDrink > 18) {
+        console.log(`Не желаете ли олд-фэшн  ${name}`);
+    } else {
+        console.log(`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`);
+    }
+
 }
